bdb0345f8fe3c14e1b36813a9029e57a3130a76f
ngquangvu <nguyenquangvu.work@gmail.com>
2023-10-03 22:30:12 +0700

    add pagination response data

--- src/users/controllers/users.admin.controller.ts
+++ src/users/controllers/users.admin.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Request, Post, Body, Bind, UseGuards, Get, Param, Query } f
 import { UserService } from '../services/users.service'
 import { JwtAdminAuthGuard } from 'src/token/guards'
 import { log } from 'console'
+import { PaginationQueryDto } from 'src/common/dtos'
 
 @Controller('/admin/users')
 @UseGuards(JwtAdminAuthGuard)
@@ -9,8 +10,8 @@ export class AdminUsersController {
     constructor(private readonly _userService: UserService) {}
     @Get()
     @Bind(Request())
-    async getUsers() {
-        return await this._userService.findAll()
+    async getUsers(@Query() userPaginationQuery: PaginationQueryDto) {
+        return await this._userService.findAll(userPaginationQuery)
     }
 
     @Get(':id')
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from 'src/prisma/service/prisma.service'
 import { UpdateUserDto } from '../dtos'
 import { AuthenticationProvider } from 'src/authentication/providers'
 import { RegisterUserDto } from 'src/authentication/dtos'
+import { PaginationQueryDto } from 'src/common/dtos'
+import { Prisma } from '@prisma/client'
 
 @Injectable()
 export class UserService {
@@ -36,11 +38,42 @@ export class UserService {
         return user
     }
 
-    async findAll() {
-        return this._prismaService.user.findMany({
-            where: {},
-            ...this._select
-        })
+    async findAll(userPaginationQuery: PaginationQueryDto) {
+        const { page = 1, limit = 10, search = undefined } = userPaginationQuery
+
+        const or = search
+            ? {
+                  OR: [
+                      { firstName: { contains: search } },
+                      { lastName: { contains: search } },
+                      { email: { contains: search } }
+                  ]
+              }
+            : {}
+
+        const [totalCount, data] = await Promise.all([
+            this._prismaService.user.count({
+                where: {
+                    ...or,
+                    deletedAt: null
+                }
+            }),
+            this._prismaService.user.findMany({
+                skip: (page - 1) * limit,
+                take: limit,
+                where: {
+                    ...or,
+                    deletedAt: null
+                },
+                orderBy: { updatedAt: Prisma.SortOrder.desc },
+                ...this._select
+            })
+        ])
+
+        return {
+            data,
+            totalCount
+        }
     }
 
     async userRegister(registerUserDto: RegisterUserDto) {
